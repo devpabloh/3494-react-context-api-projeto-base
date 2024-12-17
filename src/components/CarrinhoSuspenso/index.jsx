93c80caa5fd671c699e29da5552c26c5031906fd
pablo <devpabloh@gmail.com>
2024-12-17 09:07:53 -0300

    Criando hook

--- src/components/CarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/index.jsx
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Botao from "@/components/Botao";
 import ListaProdutosCarrinho from "@/components/ListaProdutosCarrinho";
 import TotalCarrinho from "./TotalCarrinho";
 import Titulo from "@/components/Titulo";
+import {CarrinhoContext} from "@/context/CarrinhoContext"
 
-const CarrinhoSuspenso = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-}) => {
+const CarrinhoSuspenso = () => {
+  const {carrinho} = useContext(CarrinhoContext);
   return (
     <div
       className="offcanvas offcanvas-end text-bg-dark"
@@ -35,12 +31,8 @@ const CarrinhoSuspenso = ({
       </div>
       <div className="offcanvas-body">
         <ListaProdutosCarrinho
-          carrinho={carrinho}
-          adicionarProduto={adicionarProduto}
-          removerProduto={removerProduto}
-          removerProdutoCarrinho={removerProdutoCarrinho}
-        />
-        <TotalCarrinho valorTotalCarrinho={valorTotalCarrinho} />
+          carrinho={carrinho}/>
+       {/*  <TotalCarrinho valorTotalCarrinho={valorTotalCarrinho} /> */}
       </div>
     </div>
   );
